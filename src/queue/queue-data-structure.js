ba485cecc7bc20e8f7e91d3c615939c85808b353
raulfabra <fabraraul@yahoo.com>
2023-04-13 20:04:15 +0200

    lab done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,32 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length === 0) return true
+    if (this.queueControl.length === this.MAX_SIZE) return false
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.queueControl.length === 0) return true
+    else return false
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.queueControl.length === this.MAX_SIZE) throw new Error('QUEUE_OVERFLOW');
+    this.queueControl.push(item)
+    return this.queueControl
   }
 
   dequeue() {
     // ... your code goes here
+    if (this.queueControl.length === 0) throw new Error('QUEUE_UNDERFLOW');
+    return this.queueControl.shift()
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl
   }  
 }
 
